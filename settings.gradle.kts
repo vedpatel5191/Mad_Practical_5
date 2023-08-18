5f472f1bbb0025dd5c5b536aeba729d6f217d27f
vedpatel5191 <ved2889@gmail.com>
2023-08-18 12:24:57 +0530

    Initial commit

---
+++ settings.gradle.kts
@@ -0,0 +1,17 @@
+pluginManagement {
+    repositories {
+        google()
+        mavenCentral()
+        gradlePluginPortal()
+    }
+}
+dependencyResolutionManagement {
+    repositoriesMode.set(RepositoriesMode.FAIL_ON_PROJECT_REPOS)
+    repositories {
+        google()
+        mavenCentral()
+    }
+}
+
+rootProject.name = "Mad Pra-5_90_5B6"
+include(":app")
